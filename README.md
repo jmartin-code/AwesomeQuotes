9901619c2ee0bfd48925c56d5f732720a61291fc
jmartin-code <jmmartinez331@gmail.com>
2021-04-30 14:48:09 -0400

    updated readme file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #AWESOME QUOTES
 
-This is an app that I developed using single-page React.
+This is a simple app that I developed using single-page React.
 The app is currently connected to Firebase database to save and pull quotes entered by users.
---
+++ build/static/js/7.8af2b041.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[7],{53:function(e,t,c){"use strict";c.r(t);var i=c(1);t.default=function(){return Object(i.jsx)("div",{className:"centered",children:Object(i.jsx)("p",{children:"This page is not available!"})})}}}]);
+//# sourceMappingURL=7.8af2b041.chunk.js.map
